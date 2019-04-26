f35abc59bb07cde59b1415dd86b945b450105e90
Gijs Vermariën <gijs.vermarien@gmail.com>
2019-04-26 19:12:55 +0200

    Created the marktex script

---
+++ compile.sh
@@ -0,0 +1,16 @@
+# /bin/bash
+notforconversion="readme.md"
+echo $notforconversion is excluded from the conversion
+markdownfiles=$(find ./ -iname "*.md" -and ! -name $notforconversion)
+echo Trying to convert: $markdownfiles
+for markdownfile in $markdownfiles; do
+	latextarget="$(echo $markdownfile | sed -e 's/.md/.tex/')"
+	if [ $latextarget -ot $markdownfile ]; then
+		latextarget="$(echo $markdownfile | sed -e 's/.md/.tex/')"
+		pandoc -f markdown -t latex $markdownfile >>$latextarget
+	echo "converted" $markdownfile "to" $latextarget
+	else
+		echo $markdownfile did not need converting as $latextarget is newer
+	fi
+done
+echo "done"
---
+++ readme.md
@@ -0,0 +1,3 @@
+# marktex
+This is a small tool that writes converts all the markdown files in this folder to
+a LaTeX .tex files with the corresponding name. It will not convert the readme.md file
